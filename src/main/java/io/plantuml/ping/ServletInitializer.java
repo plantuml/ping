0c257d47cf757d80ebeedf9ea2c4b00dbb940477
Arnaud Roques <plantuml@gmail.com>
2023-03-27 19:19:46 +0200

    feat: add /ping endpoint

---
+++ src/main/java/io/plantuml/ping/ServletInitializer.java
@@ -0,0 +1,13 @@
+package io.plantuml.ping;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PingApplication.class);
+	}
+
+}
