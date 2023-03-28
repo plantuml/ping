97de934e399a39fe5dd9842e79612d36ec381a26
PlantUML <arnaud_roques@yahoo.com>
2023-03-28 16:30:20 +0200

    refactor: simplify PingApplication

--- src/main/java/io/plantuml/ping/PingApplication.java
+++ src/main/java/io/plantuml/ping/PingApplication.java
@@ -2,12 +2,19 @@ package io.plantuml.ping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PingApplication {
+public class PingApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PingApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(io.plantuml.ping.PingApplication.class);
 	}
 
 }
