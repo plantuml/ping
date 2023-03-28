797759b449286ac280b514d1468407bf33e7a6f3
PlantUML <arnaud_roques@yahoo.com>
2023-03-28 19:30:37 +0200

    feat: add /ping context instead of root context

--- src/main/java/io/plantuml/ping/PingApplication.java
+++ src/main/java/io/plantuml/ping/PingApplication.java
@@ -3,7 +3,10 @@ package io.plantuml.ping;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PingApplication extends SpringBootServletInitializer {
@@ -15,6 +18,11 @@ public class PingApplication extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(io.plantuml.ping.PingApplication.class);
+	}
+
+	@Bean
+	public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> webServerFactoryCustomizer() {
+		return factory -> factory.setContextPath("/ping");
 	}
 
 }
